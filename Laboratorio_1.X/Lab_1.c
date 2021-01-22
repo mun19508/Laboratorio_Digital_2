a0f8f76ed507637b11cad4a5fb3548dd31d4d9a8
mun19508 <68123854+mun19508@users.noreply.github.com>
2021-01-21 23:07:17 -0600

    Lab_1_v1
    -Se configuro como salida el puerto A,C, D & E.
    -Se configuro el puerto B como entrada, además se habilito las pull up internas.

---
+++ Laboratorio_1.X/Lab_1.c
@@ -0,0 +1,49 @@
+/*
+ * File:   Lab_1.c
+ * Author: Daniel
+ *
+ * Created on 21 de enero de 2021, 06:08 PM
+ */
+
+
+#include <xc.h>
+// CONFIG1
+#pragma config FOSC = INTRC_NOCLKOUT// Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown Out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+
+// CONFIG2
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+
+//**************************Prototipos de Funciones*****************************
+void config(void);
+void start_race(void);
+//******************************************************************************
+void main(void) {
+    config();
+    
+    while(1){
+    }
+}
+void config(void){
+    ANSEL = 0;
+    ANSELH = 0; //Puerto A & B como digitales
+    TRISA = 0;
+    TRISC = 0;
+    TRISD = 0;
+    TRISE = 0; //Puerto A, C, D & E como salidas
+    TRISB = 1; //Puerto B como entrada 
+    PORTA = 0;
+    PORTC = 0;
+    PORTD = 0;
+    PORTE = 0; //Se limpian los puertos
+    OPTION_REGbits.nRBPU = 0; //Se habilita el pull up interno       
+}

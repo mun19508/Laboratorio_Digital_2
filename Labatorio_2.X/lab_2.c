d36fee165e7365e72d76a7660b9d939f0e14cd8c
mun19508 <68123854+mun19508@users.noreply.github.com>
2021-01-28 20:56:54 -0600

    Laboratorio 2
    Primera subida, falta:
    -Primera parte          -Tercera parte
    -Segunda parte        -Cuarta parte

---
+++ Labatorio_2.X/lab_2.c
@@ -0,0 +1,13 @@
+/*
+ * File:   lab_2.c
+ * Author: Daniel
+ *
+ * Created on 28 de enero de 2021, 05:36 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ Labatorio_2.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Labatorio_2.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Labatorio_2.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=labatorio2.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/labatorio2.x.tar

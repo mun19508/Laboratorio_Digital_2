96ad8ace91eec817083032b954fe4fc44ec3fb35
mun19508 <68123854+mun19508@users.noreply.github.com>
2021-05-06 12:04:39 -0600

    Parte 3
    completa

--- Experimento_1/main.c
+++ Experimento_1/main.c
@@ -14,6 +14,9 @@
 /**************************
  * Prototipo de funciones *
  **************************/
+void semaforo(void);
+void delayManual(void);
+/**************************/
 int main(void)
 {
 //Parte 2: Configuración del reloj
@@ -24,4 +27,31 @@ int main(void)
     SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
     //Parte 3: Configuracion de Puertos
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);   //habilita el puerto F
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1); //se habilitan como salida los pines de los leds (ledG|ledB|ledR)
+    semaforo();//funcion de semaforo version 1
 }
+/*******Subrutinas*******/
+void semaforo(void){
+    GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1, 2); //enciende led rojo
+    delayManual();
+    GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1, 0); //apaga led
+    delayManual();
+    GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1, 10); //enciende led amarillo
+    delayManual();
+    GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1, 0); //apaga led
+    delayManual();
+    GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1, 8); //enciende led verde
+    delayManual();
+    int i;
+    for(i = 0; i < 8; i++){
+        delayManual();
+        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1, 0); //apaga led
+        delayManual();
+        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1, 8); //enciende led verde
+    }
+}
+void delayManual(void){
+    uint32_t j;
+    for(j=0; j<2000000; j++){}
+}
+

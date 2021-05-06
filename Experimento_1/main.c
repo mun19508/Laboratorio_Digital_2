6d58acbaeac9408eb52d7abfd312741eaafad9a8
mun19508 <68123854+mun19508@users.noreply.github.com>
2021-05-06 01:21:47 -0600

    un nuevo comienzo

---
+++ Experimento_1/main.c
@@ -0,0 +1,20 @@
+/*UVG
+ *Autor: Daniel Mundo
+ *Carnet:19508
+ *Nombre del archivo: main.c
+ */
+/**************************
+ * Librerias              *
+ **************************/
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+/**************************
+ * Prototipo de funciones *
+ **************************/
+int main(void)
+{
+	return 0;
+}

29b7c76a35879c0a4ed83b403c7a58737764f361
mun19508 <68123854+mun19508@users.noreply.github.com>
2021-05-06 17:37:51 -0600

    Comienzo
    primer commit

---
+++ Experimento_2/main.c
@@ -0,0 +1,26 @@
+/*UVG
+ *Autor: Daniel Mundo
+ *Carnet:19508
+ *Nombre del archivo: main.c
+ */
+/********************************************
+ *              Librerias                   *
+ ********************************************/
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_ints.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/gpio.h"
+#include "driverlib/timer.h"
+/********************************************
+ *           Prototipo de funciones         *
+ ********************************************/
+
+int main(void)
+{
+    return 0;
+}

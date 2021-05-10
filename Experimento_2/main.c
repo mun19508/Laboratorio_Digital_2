48c5e45ed026db504a72310a805375350bda7d27
mun19508 <68123854+mun19508@users.noreply.github.com>
2021-05-09 22:27:55 -0600

    Parte 2 casi completa

--- Experimento_2/main.c
+++ Experimento_2/main.c
@@ -1,9 +1,9 @@
-/*UVG
- *Autor: Daniel Mundo
- *Carnet:19508
- *Nombre del archivo: main.c
- */
 /********************************************
+ *Autor: Daniel Mundo                       *
+ *Carnet:19508                              *
+ *Nombre del archivo: main.c                *
+ *Archivo compilable                        *
+ ********************************************
  *              Librerias                   *
  ********************************************/
 #include <stdint.h>
@@ -19,8 +19,20 @@
 /********************************************
  *           Prototipo de funciones         *
  ********************************************/
-
+void toggle1Hz(void);
+/********************************************/
 int main(void)
-{
-    return 0;
+{   //Parte 2:
+    SysCtlClockSet(
+            SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ
+                    | SYSCTL_OSC_MAIN); //Config. del reloj a 40 MHz
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF|SYSCTL_PERIPH_TIMER0);   //Habilita el puerto F & el timer 0
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE,
+                          GPIO_PIN_3 | GPIO_PIN_2 | GPIO_PIN_1); //Los pines de los habilitados como salidas
+    TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
+
+//    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
 }
+/********************************************
+ *              Subrutinas                  *
+ ********************************************/
